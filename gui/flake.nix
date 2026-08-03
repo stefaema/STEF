3b0067191488dd38427a6e5743eb0daedf0930fa
Fernando Stefanovic <stefanovicfernando@gmail.com>
2026-08-03 15:40:18 -0300

    gui: provision dev shell for FastAPI frontend

--- gui/flake.nix
+++ gui/flake.nix
@@ -11,10 +11,20 @@
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
     in {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          buildInputs = [ pkgs.python3 ];
-        };
-      });
+      devShells = forAllSystems (pkgs:
+        let
+          python = pkgs.python3.withPackages (ps: [
+            ps.fastapi
+            ps.uvicorn
+            ps.jinja2
+            ps.httpx
+            ps.pytest
+            ps.pytest-asyncio
+          ]);
+        in {
+          default = pkgs.mkShell {
+            buildInputs = [ python pkgs.gettext ];
+          };
+        });
     };
 }
